dc61ba00c730d953ebbf0ef934ee1d09e901c1b3
root <sandrineyonb@gmail.com>
2023-06-19 10:28:02 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main(void)
-{       puts("\"Programming is like building a multilingual puzzle");
-	returm (0);
+{puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
