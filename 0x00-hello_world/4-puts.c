dac9254415647d73ea95d95a4dc98fbc33520ac2
root <sandrineyonb@gmail.com>
2023-06-19 11:55:13 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
- *main - Entry point
- *
- * * Description : print a sentence
- * @parameter :
- *
- * * Return: Always 0(success)
- */
+ *  *Description:main-prints out a string to stdout.
+ *  *Return:0 if success
+*/
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
+{
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
