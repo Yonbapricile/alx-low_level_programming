d946e8312e94af5574ec28ab353438c9cb07731b
root <sandrineyonb@gmail.com>
2023-06-19 10:19:47 -0700

    first C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int main(voidd)
-{       puts ("\"Programming is like building a multilingual puzzle);
+int main(void)
+{       puts("\"Programming is like building a multilingual puzzle");
 	retutn (0)
 }
