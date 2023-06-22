66cc079f9d35336f922083e281736013fb3019e8
root <sandrineyonb@gmail.com>
2023-06-21 17:03:31 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - completes the absolute value
+ * of an interger
+ * Description: completes the absolute value
+ * @int:
+ * input value of an interger
+ *
+ * Return: Absolute value.
+ */
+int _abs(int);
+{
+if (i >= 0)
+{
+return (i);
+}
+else
+{
+return (i = -1);
+}
+}
